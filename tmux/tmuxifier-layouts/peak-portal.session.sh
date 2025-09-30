2ef35feb7d6a64f23ee2051671adc96e0c81a3e6
Robert Hill <robert.hill@uphillsolutions.tech>
2025-09-30 07:36:44 -0400

    feat: add layouts for tmuxifier

---
+++ tmux/tmuxifier-layouts/peak-portal.session.sh
@@ -0,0 +1,26 @@
+# layouts/peak-portal.session.sh
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "$HOME/Documents/github-uphill/peak/"
+# Check if tmux is running, and start it if not
+if ! pgrep tmux >/dev/null; then
+  tmux start-server
+fi
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "uphill"; then
+  new_window ""
+  run_cmd "cd $session_root"
+  run_cmd "yazi"
+  new_window ""
+  run_cmd "cd $session_root"
+  run_cmd "claude"
+  new_window ""
+  run_cmd "cd $session_root"
+  run_cmd "npm run dev"
+  select_window 1
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
---
+++ tmux/tmuxifier-layouts/system_update.sh
@@ -0,0 +1,25 @@
+# layouts/system_update.sh
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "$HOME"
+# Check if tmux is running, and start it if not
+if ! pgrep tmux >/dev/null; then
+  tmux start-server
+fi
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "uphill"; then
+  new_window ""
+  run_cmd "cd $session_root"
+  run_cmd "sysup"
+  split_v 33
+  run cmd "cd $session_root" 1
+  run cmd "journalctl -f"
+  # new_pane "terminal"
+  # run_cmd "cd $session_root"
+  select_window 1
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
