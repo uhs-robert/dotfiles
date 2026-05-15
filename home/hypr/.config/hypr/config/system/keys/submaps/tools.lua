fae7891dc2691491e8b0fec39bc2fd6f6bbcbe31
Robert Hill <robert.hill@uphillsolutions.tech>
2026-05-14 23:25:21 -0400

    refactor(hypr/tools-submap): use Scripts module for screenshot path

--- home/hypr/.config/hypr/config/system/keys/submaps/tools.lua
+++ home/hypr/.config/hypr/config/system/keys/submaps/tools.lua
@@ -2,26 +2,25 @@
 
 local SubBind = require("lib.submap_bind") ---@class SubBind
 local SUBMAP = require("config.system.keys.submap").map
+local Scripts = require("lib.scripts") ---@class Scripts
 
 --- Screenshot
 hl.define_submap(SUBMAP.screenshot.name, function()
-  local SS = "~/.config/hypr/scripts/screenshot.sh"
-    -- stylua: ignore start
-  --
-    -- !--- Screenshot modes ---
-    SubBind.run("SLASH",     SS,                        "Print Options")
-    SubBind.run("I",         SS .. " --freeze",         "Screenshot Frozen Region")
-    SubBind.run("SHIFT + I", SS .. " --record-region",  "Record Region")
-    SubBind.run("S",         SS .. " --screen",         "Screenshot Screen")
-    SubBind.run("SHIFT + S", SS .. " --record-screen",  "Record Screen")
-    SubBind.run("W",         SS .. " --window",         "Screenshot Window")
-    SubBind.run("SHIFT + W", SS .. " --record-window",  "Record Window")
-    SubBind.run("R",         SS .. " --record-focused", "Record Focused Window")
+  -- stylua: ignore start
+  local SS = Scripts.screenshot
+  -- !--- Screenshot modes ---
+  SubBind.run("SLASH",     SS,                        "Print Options")
+  SubBind.run("I",         SS .. " --freeze",         "Screenshot Frozen Region")
+  SubBind.run("SHIFT + I", SS .. " --record-region",  "Record Region")
+  SubBind.run("S",         SS .. " --screen",         "Screenshot Screen")
+  SubBind.run("SHIFT + S", SS .. " --record-screen",  "Record Screen")
+  SubBind.run("W",         SS .. " --window",         "Screenshot Window")
+  SubBind.run("SHIFT + W", SS .. " --record-window",  "Record Window")
+  SubBind.run("R",         SS .. " --record-focused", "Record Focused Window")
 
-    -- !--- Tools ---
-    SubBind.run("T", SS .. " --text",  "OCR Text in Region")
-    SubBind.run("P", SS .. " --pixel", "Color Picker")
-  -- stylua: ignore end
+  -- !--- Tools ---
+  SubBind.run("T", SS .. " --text",  "OCR Text in Region")
+  SubBind.run("P", SS .. " --pixel", "Color Picker")
 
   SubBind.bind_exits()
 end)
