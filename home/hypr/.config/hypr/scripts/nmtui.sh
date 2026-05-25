33b7a8d9675536c09a942ff14d6576b986ff99e7
Robert Hill <robert.hill@uphillsolutions.tech>
2026-05-25 16:03:14 -0400

    feat(waybar): open nmtui-connect on network click
    Add --connect flag to nmtui.sh so callers can choose nmtui-connect
    vs nmtui without separate scripts. Waybar left-click uses --connect;
    middle-click opens full nmtui.

--- home/hypr/.config/hypr/scripts/nmtui.sh
+++ home/hypr/.config/hypr/scripts/nmtui.sh
@@ -2,8 +2,14 @@
 
 export NEWT_COLORS='root=,black window=white,black border=brown,black title=red,black textbox=white,black label=white,black entry=red,black disentry=gray,black button=black,cyan actbutton=black,cyan compactbutton=green,black listbox=white,black actlistbox=red,black sellistbox=white,green actsellistbox=black,cyan checkbox=white,black actcheckbox=black,cyan emptyscale=gray,black fullscale=black,green helpline=green,black roottext=green,black'
 
-if [[ "$1" == "--waybar" ]]; then
-  exec foot --app-id waybar-nmtui -e nmtui
-else
-  exec foot -e nmtui "$@"
-fi
+FOOT_ARGS=()
+CMD="nmtui"
+
+for arg in "$@"; do
+  case "$arg" in
+    --waybar)  FOOT_ARGS+=(--app-id waybar-nmtui) ;;
+    --connect) CMD="nmtui-connect" ;;
+  esac
+done
+
+exec foot "${FOOT_ARGS[@]}" -e "$CMD"
