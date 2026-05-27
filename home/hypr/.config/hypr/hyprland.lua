970f1f8f24851f045e2e774d83d04e11597ac013
Robert Hill <robert.hill@uphillsolutions.tech>
2026-05-27 15:24:01 -0400

    chore(hypr): swap monitor config from HP Z22n to Lenovo T22i-30

--- home/hypr/.config/hypr/hyprland.lua
+++ home/hypr/.config/hypr/hyprland.lua
@@ -14,7 +14,7 @@ local Config = require("config") ---@class Config
 -- stylua: ignore start
 local MONITORS_LAPTOP = {
     { description = "BOE 0x0C8E",                                        mode = "2560x1600@240",    position = "317x1080", scale = 1.6 },
-    { description = "HP Inc. HP Z22n G2 6CM8411J1Z",                     mode = "1920x1080@60",     position = "0x0",      scale = 1 },
+    { description = "Lenovo Group Limited T22i-30 V81037ZT",             mode = "1920x1080@60",     position = "0x0",      scale = 1 },
     { description = "GIGA-BYTE TECHNOLOGY CO. LTD. G27QC A 0x00000D48",  mode = "2560x1440@143.97", position = "1920x0",   scale = 1 },
     { description = "HP Inc. HP Z22n G2 6CM8411J22",                     mode = "1920x1080@60",     position = "4480x0",   scale = 1,   transform = 3 },
 }
