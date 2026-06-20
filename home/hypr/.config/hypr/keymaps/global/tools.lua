001a4fe83e84872ddba066df420ab4a85ee46267
Robert Hill <robert.hill@uphillsolutions.tech>
2026-06-20 14:17:08 -0400

    feat(hypr): add Alt+Tab window finder binding

--- home/hypr/.config/hypr/keymaps/global/tools.lua
+++ home/hypr/.config/hypr/keymaps/global/tools.lua
@@ -24,6 +24,7 @@ Bind.leader_cmd("CTRL + V", cmd_search_clipboard, "Clipboard History")
 -- Window Selector / Move
 local function select_window(action) return Scripts.window_selector .. " --" .. action end
 Bind.leader_fn("T",                 Menu.hyprwindow(),               "Find window")
+Bind.fn("ALT + TAB",                Menu.hyprwindow(),               "Find window")
 Bind.leader_cmd("SHIFT + T",        select_window("move"),           "Move to another window")
 Bind.leader_cmd("CTRL + SHIFT + T", select_window("move-silent"),    "Silent move to another window")
 
