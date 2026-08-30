875a0e6efd544bc9dcb7d6592ff0dc23d6471d8e
Robert <robert.hill@uphillsolutions.tech>
2026-08-30 14:47:53 -0400

    feat(hypr): make group submap Vim-oriented

--- home/hypr/.config/hypr/keymaps/submaps/groups/init.lua
+++ home/hypr/.config/hypr/keymaps/submaps/groups/init.lua
@@ -15,11 +15,24 @@ Submap.define({
   binds = function()
     return {
       { "SPACE", Window.group_toggle(), "Toggle Group" },
-      { "J", Window.group_next(), "Next Group Window" },
-      { "K", Window.group_prev(), "Previous Group Window" },
-      { "H", Window.group_move_out(), "Move Out of Group" },
-      { "L", Window.group_move_in(), "Move Into Group" },
-      { "X", Window.group_lock_toggle(), "Toggle Group Lock" },
+
+      { "H", Window.focus_dir("l"), "Focus Left" },
+      { "J", Window.focus_dir("d"), "Focus Down" },
+      { "K", Window.focus_dir("u"), "Focus Up" },
+      { "L", Window.focus_dir("r"), "Focus Right" },
+
+      { "SHIFT + H", Window.group_move_in("l"), "Move Into Group Left" },
+      { "SHIFT + J", Window.group_move_in("d"), "Move Into Group Down" },
+      { "SHIFT + K", Window.group_move_in("u"), "Move Into Group Up" },
+      { "SHIFT + L", Window.group_move_in("r"), "Move Into Group Right" },
+
+      { "TAB", Window.group_next(), "Next Group Window" },
+      { "SHIFT + TAB", Window.group_prev(), "Previous Group Window" },
+      { "BRACKETRIGHT", Window.group_next(), "Next Group Window" },
+      { "BRACKETLEFT", Window.group_prev(), "Previous Group Window" },
+
+      { "X", Window.group_move_out(), "Move Out of Group" },
+      { "SHIFT + X", Window.group_lock_toggle(), "Toggle Group Lock" },
     }
   end,
 }).setup()
